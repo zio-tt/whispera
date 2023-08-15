18bf79ec403a0cd9ea7f57f4e540885bb577613f
terao_tomohiro_44 <zio.tt.dev@gmail.com>
2023-08-15 15:57:40 +0900

    validation title 18

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,7 +1,7 @@
 class Episode < ApplicationRecord
   has_many :likes
 
-  validates :title, presence: true, length: { maximum: 10 }
+  validates :title, presence: true, length: { maximum: 18 }
   validates :theme, presence: true
   validates :content, presence: true, length: { maximum: 360 }
 
